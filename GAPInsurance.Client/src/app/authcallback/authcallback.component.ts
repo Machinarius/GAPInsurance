8516cfdd424e9555f7cb93067030a2523a9026c9
Germán Valencia <machinarius92@gmail.com>
2018-05-09 08:35:28 -0500

    More auth progress

--- GAPInsurance.Client/src/app/authcallback/authcallback.component.ts
+++ GAPInsurance.Client/src/app/authcallback/authcallback.component.ts
@@ -1,9 +1,17 @@
 import { Component } from "@angular/core";
+import { OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   templateUrl: './authcallback.component.html',
   styleUrls: ['../../styles/herocard.scss']
 })
-export class AuthCallbackComponent {
+export class AuthCallbackComponent implements OnInit {
+  constructor(
+    private authService: AuthService
+  ) { }
 
+  public async ngOnInit(): Promise<any> {
+    await this.authService.finishLogin();
+  }
 }
--- GAPInsurance.Client/src/app/landing/landing.component.ts
+++ GAPInsurance.Client/src/app/landing/landing.component.ts
@@ -1,9 +1,19 @@
 import { Component } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
     templateUrl: './landing.component.html',
     styleUrls: ['./landing.component.scss', '../../styles/herocard.scss']
 })
 export class LandingComponent {
+  public username: string;
+  public password: string;
 
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  public async login(): Promise<any> {
+    await this.authService.beginLogin(this.username, this.password);
+  }
 }
