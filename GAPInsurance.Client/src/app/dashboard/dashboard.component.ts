03381b571abf4ffd936559b28254fd15140f4e6e
Germán Valencia <machinarius92@gmail.com>
2018-05-09 23:40:38 -0500

    Early dashboard templates

---
+++ GAPInsurance.Client/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['../../styles/maincolumn.scss', './dashboard.component.scss']
+})
+export class DashboardComponent {
+
+}
